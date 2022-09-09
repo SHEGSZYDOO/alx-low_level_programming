f4f7342ace39b0a2126b9ad0227e17bcb801fabb
SHEGSZYDOO <segunbenson123@gmail.com>
2022-09-08 23:33:06 -0700

    coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,21 @@
-#!/bin/bash
 #include <stdio.h>
+
+/**                                                                                                                                   
+
+*main - print the string in the put fucntion                                                                                          
+
+*                                                                                                                                     
+
+*Description: using the main function                                                                                                 
+
+*this program prints "Program is like building a multilingual puzzle                                                                  
+
+*Return: 0                                                                                                                            
+
+*/int main(void)         
+{
+
+   	  puts("\"programming is like a building a multilingual puzzle");                                                 
+
+          return (0);
+}
