c4c0db2423c049e94d4a086bdec8cc10adc4c53d
SHEGSZYDOO <segunbenson123@gmail.com>
2022-09-09 10:33:59 -0700

    coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,13 +6,11 @@
 
  * main - Entry point
 
- * Description: prints "\"Programming is like building a multilingual puzzle"
+ *
 
- * Return: 0
+ * Return: Always 0 (Success)
 
  */
-
-
 
 int main(void)
 
@@ -21,4 +19,5 @@ int main(void)
 	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
+
 }
