93c6c9d703c5cdc1775ae14f6498b6e71e3240cf
SHEGSZYDOO <segunbenson123@gmail.com>
2022-09-16 00:29:44 -0700

    fizz buzz file to be committed

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+#include <stdio.h>
 
 /**
-*main - program that prints either number
-*or fizz or buzz or fizzBuzz
-*
-*
-*Return: returns 0
-*/
+ * main - program that prints either number
+ * or fizz or buzz or fizzBuzz
+ *
+ *
+ * Return: returns 0
+ */
 
 int main(void)
 {
