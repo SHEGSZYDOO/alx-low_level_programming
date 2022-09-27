ffbe2c2b6aecf9ec85e8035fefa4352be4e247a5
SHEGSZYDOO <segunbenson123@gmail.com>
2022-09-27 13:36:42 -0700

    3 strspn file to be committed

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -4,12 +4,13 @@
  * _strspn - gets lengthof a prefix substring
  * @s: string to check
  * @accept: string to check against
+ *
  * Return: number of bytes of s in accept
  */
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i, j;
-	
+
 	for (i = 0; s[i]; i++)
 	{
 		for (j = 0; accept[j]; j++)
