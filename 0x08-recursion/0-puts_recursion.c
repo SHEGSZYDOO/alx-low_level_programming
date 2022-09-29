3751d1aa53779924e1ba413cdbc41a3699909a07
SHEGSZYDOO <segunbenson123@gmail.com>
2022-09-29 02:39:22 -0700

    0 put recursion t file be committed

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,14 +7,14 @@
 * Return: void
 */
 void _puts_recursion(char *s)
-{
-        if (*s == '\0')
-        {
-                _putchar('\n');
-        }
-        else
-        {
-                _putchar(s[0]);
-                _puts_recursion(s + 1);
-        }
+{  	
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(s[0]);
+		_puts_recursion(s + 1);
+	}
 }
