8684f1f17105334e8c16879e8ac0fc0bec60b7ac
SHEGSZYDOO <segunbenson123@gmail.com>
2022-09-28 18:05:39 -0700

    put recursion to be committed

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,13 +8,13 @@
  */
 void _puts_recursion(char *s)
 {
-        if (*s == '\O')
+        if (*s == '\0')
         {
                 _putchar('\n');
         }
         else
         {
-                _putchar(s[O]);
+                _putchar(s[0]);
                 _puts_recursion(s + l);
         }
 }
