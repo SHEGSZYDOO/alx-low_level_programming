275c4aae46b260caca25ab0d12de5643cec364f2
SHEGSZYDOO <segunbenson123@gmail.com>
2022-10-05 05:28:02 -0700

    0 create array file to be committed

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,16 +2,16 @@
 
 /**
 * create_array - creates an array of chars, and
-* initializes with a specific char.
+* initializes it with a specific char.
 *@size: Size of the array
 *@c: Character to insert
-*Return: Null if size is zero or if it fails,
+*Return: NULL if size is zero or if it fails,
 *pointer to array if everything is normal.
 */
 
 char *create_array(unsigned int size, char c)
 {
-	char *array:
+	char *array;
 	unsigned int index;
 
 	if (size == 0)
